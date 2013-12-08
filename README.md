c87ea947ec75e84e731a43dd13e9d95572d5c8ba
Anomitee <Anomitee@gmail.com>
2013-12-08 19:38:28 +1100

    Added GitHub Fork update script

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Scripts
 * [Viginère Cipher](viginere.sh)
 
 * [Re-written Viginère Cipher](viginere2.sh)
+
+* [GitHub Forked Repository Updater](github-fork-updater.sh0
---
+++ github-fork-updater.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# GitHub forked repository updater Bash script
+# By Anomitee
+
+# This script fetches 
+# It assumes that you have added the original repository as a remote named "upstream".
+# For more information on forks, visit https://help.github.com/articles/fork-a-repo
+
+# Put this script in the directory that contains your forked repository.
+# It is recommended to run this in a terminal
+
+# Go to the directory the script is in
+cd $(dirname $0)
+
+# Fetch changes made upstream
+git fetch upstream
+
+# Merge these changes.
+# Uncomment this line to have the script merge the changes automatically
+# git merge upstream/master
