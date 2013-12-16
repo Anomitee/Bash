b114dd92eb5ed86438d9bd5211406cb10fe94ffb
Anomitee <Anomitee@gmail.com>
2013-12-17 10:57:23 +1100

    Spaced out command to make it easier to copy/paste

---
+++ scramble.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# String scrambler
+# Scrambles a string and 
+# By Anomitee
+
+# Function for easy copy-pasting into other scripts
+scramble() {
+  string=$1                                     # Obtain string to scramble
+  while [[ -n "$string" ]]; do                  # Whilst the string is still of non-zero length
+    length=${#string}                           # get the lenght of the string
+    char=${string:$((RANDOM%$length)):1}        # Get a random character from the string
+    buffer=$buffer$char                         # Save it to a "buffer" variable
+    string=$(printf "$string" | sed s/$char//)  # Delete it from the string
+  done
+  string=$buffer    # Replace the string with its scrambled version
+  printf "$string"  # Print string (no newline for more flexibility)
+}
+scramble $1
