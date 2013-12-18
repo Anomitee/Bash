ef056f5e7647cef984a406ec378fef1583f7163b
Anomitee <Anomitee@gmail.com>
2013-12-18 19:07:41 +1100

    Completed basic fuctionality

--- enigma/enigma.sh
+++ enigma/enigma.sh
@@ -13,9 +13,9 @@ numr=3
 for r in $(seq $numr); do
   rot=rot$r
   let l$rot=$(expr length ${!rot})
-  if [[ "$(($l$r%2))" == 1 ]]; then
-    printf "$rot has an odd number of characters."
-    printf "Please edit the script and fix this."
+  if [[ "$((lrot$r%2))" == 1 ]]; then
+    printf "$rot has an odd number of characters.\n"
+    printf "Please edit the script and fix this.\n"
     exit 1
   fi
   let step$rot=0
@@ -33,8 +33,8 @@ cipher() {
 
 turn() {
   step=$((step+1))
-  for n in $(seq $numr -1 1)
-    total=$((lrot`seq -s *lrot $n -1 1`))
+  for n in $(seq $numr -1 1); do
+    total=lrot$((seq -s '*lrot' $n -1 1))
     if [[ "$step" -ge "$total" ]]
     then
       step=$((step-total))
@@ -43,3 +43,24 @@ turn() {
     fi
   done
 }
+
+read -t 0
+if [[ "$?" == 0 ]]
+then
+  read message
+else
+  printf "Type message.\n"
+  read message
+  printf "\n"
+fi
+
+while [[ -n "$message" ]]; do
+  char=${message::1}
+  for c in rot{1..3} refl rot{3..1}; do
+    char=$(cipher $c "$char")
+  done
+  printf "$char"
+  message=${message:1}
+done
+
+printf "\n"
