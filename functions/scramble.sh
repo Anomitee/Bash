1a5d4a052e4e143a638e7f0a1532dade64393b73
Anomitee <Anomitee@gmail.com>
2013-12-22 08:59:56 +1100

    Reverted to no options

--- functions/scramble.sh
+++ functions/scramble.sh
@@ -5,18 +5,7 @@
 
 # Function for easy copy-pasting into other scripts
 scramble() {
-  opt=0
-  while getopts n flag; do
-    case $flag in
-    n)  opt=1; end="\n";;
-    esac
-  done
-  if [[ "$#" -gt "$((1+opt))" ]]; then
-    printf "More than 1 argument detected.\n"
-    printf "Quote spaces if you want all arguments to be read as 1\n"
-    printf "Otherwise, scramble each string separately"
-  fi
-  string=$((1+opt))                             # Obtain string to scramble
+  string=$1                           # Obtain string to scramble
   while [[ -n "$string" ]]; do                  # Whilst the string is still of non-zero length
     length=${#string}                           # get the lenght of the string
     char=${string:$((RANDOM%$length)):1}        # Get a random character from the string
@@ -24,7 +13,7 @@ scramble() {
     string=$(printf "$string" | sed s/$char//)  # Delete it from the string
   done
   string=$buffer      # Replace the string with its scrambled version
-  printf "$string$n"  # Print string (no newline for more flexibility)
+  printf "$string"    # Print string (no newline for more flexibility)
 }
 
 scramble $1
